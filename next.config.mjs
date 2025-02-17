7234a54243e393ece80e305c61199f4ba37b31b9
senntou <wataru_00117@icloud.com>
2025-02-17 21:20:08 +0900

    next.config修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+/* 公開時のサブディレクトリ */
+const SUB_DIRECTORY = "/62nd";
+
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV == "production";
+
 const nextConfig = {
+  basePath: isProd ? SUB_DIRECTORY : "",
   reactStrictMode: true,
   output: "export",
+  distDir: "apache" + SUB_DIRECTORY, // ビルド先のディレクトリ
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
   sassOptions: {
     includePaths: ["./src/styles"],
+  },
+  publicRuntimeConfig: {
+    basePath: isProd ? SUB_DIRECTORY : "",
   },
 };
 
